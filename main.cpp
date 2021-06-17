dc6fd484d9e620d5e74c8e5037b0740f9df1e133
Mathys YON <mathys.yon@gmail.com>
2021-06-17 16:46:48 +0200

    the main

--- main.cpp
+++ main.cpp
@@ -1,33 +1,8 @@
 #include <iostream>
 #include <QApplication>
-#include <QFileDialog>
 #include <QMessageBox>
 #include <QPushButton>
 #include <qhtml5file/qhtmlfileaccess.h>
-
-void asyncDialogExec(QDialog *dialog)
-{
-    if (dialog != NULL) {
-        dialog->setModal(true);
-        QObject::connect(
-                    dialog, &QDialog::finished,
-                    dialog, &QObject::deleteLater,
-                    Qt::UniqueConnection);
-        dialog->show();
-    }
-}
-
-QMessageBox* asyncMsgBoxInfo(
-        QWidget* parent,
-        const QString& title,
-        const QString& text,
-        QMessageBox::StandardButtons buttons)
-{
-    auto msgBox = new QMessageBox(QMessageBox::Information,
-                                  title, text, buttons, parent);
-    asyncDialogExec(msgBox);
-    return msgBox;
-}
 
 int main(int argc, char **argv){
 
@@ -35,30 +10,18 @@ int main(int argc, char **argv){
     {
         std::cout << argv[i] << std::endl;
     }
-    // std::cout << "Bonjour" << std::endl;
 
     QApplication app(argc, argv);
-#if 0
-    QFileDialog* fileDlg = new QFileDialog(nullptr);
-    fileDlg->setFileMode(QFileDialog::AnyFile);
-    fileDlg->setWindowTitle("Selection de fichier");
-    fileDlg->setNameFilter("Any files (*)");
-    asyncDialogExec(fileDlg);
- #endif
-    //QString FilePATH = QFileDialog::getOpenFileName();
-    //std::string strFilePATH = FilePATH.toStdString();
-    //std::cout << strFilePATH << std::endl;
 
     QHtmlFileAccess *htmlFileAccess = new QHtmlFileAccess;
-    //QHtmlFileAccess htmlFileAccess;
 
     auto btn = new QPushButton; //Créer un bouton => une fois cliqué, ouvre une boite de dialogue
-    btn->setText("OpenFile");
+    btn->setText("Open a File");
     btn->show();
     QObject::connect(btn, &QPushButton::clicked, [=]{
         std::cout << "Bouton Cliqué" << std::endl;
-        //htmlFileAccess->loadFsFile("*.*", "/tmp");
-        htmlFileAccess->loadFile("*.*");
+        htmlFileAccess->loadFsFile("*.*", "/tmp");
+        //htmlFileAccess->loadFile("*.*"); //??
         });
 
 //    QObject::connect(htmlFileAccess, &QHtmlFileAccess::fileDataReady, []{
@@ -67,8 +30,8 @@ int main(int argc, char **argv){
 //        fileDataReady n'est pas appelé par loadFsFile
 
     QObject::connect(htmlFileAccess, &QHtmlFileAccess::fsFileReady, [](const QString &tmpFilePath, const QString &fileName){
-        std::cout << qUtf8Printable(tmpFilePath) << std::endl;
-        std::cout << qUtf8Printable(fileName) << std::endl;
+        std::cout << qUtf8Printable(tmpFilePath) << std::endl; //Affiche le chemin du fichier
+        std::cout << qUtf8Printable(fileName) << std::endl; //Affiche le nom du fichier
         });
 
 
